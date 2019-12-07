ed4e96aff0bc608916957cb4965b15b1f502649e
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2019-12-07 23:04:56 +0100

    Test for activity switching and text input

--- TwoActivities/app/src/androidTest/java/com/example/android/twoactivities/ActivityInputOutputTest.kt
+++ TwoActivities/app/src/androidTest/java/com/example/android/twoactivities/ActivityInputOutputTest.kt
@@ -1,4 +1,48 @@
 package com.example.android.twoactivities
 
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.typeText
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
 class ActivityInputOutputTest {
+
+    @get:Rule
+    var mActivityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun activityLaunch() {
+        // Test that when send button is clicked, SecondActivity is launched and text header is visible
+        onView(withId(R.id.button_main)).perform(click())
+        onView(withId(R.id.text_header))
+                .check(matches(isDisplayed()))
+
+        // Test that when the SecondActivity is launched and the button is clicked,
+        // then the text header reply text view is visible
+        onView(withId(R.id.button_second)).perform(click())
+        onView(withId(R.id.text_header_reply)).check(matches(isDisplayed()))
+
+    }
+
+    @Test
+    fun testInputOutput() {
+        //Given a text in the main edit text
+        onView(withId(R.id.editText_main)).perform(typeText("Hello there"))
+
+        //When the send button is clicked
+        onView(withId(R.id.button_main)).perform(click())
+
+        //Then the same text is displayed in the text message edit text
+        onView(withId(R.id.text_message)).check(matches(withText("Hello there")))
+    }
 }
--- TwoActivities/app/src/androidTest/java/com/example/android/twoactivities/ExampleInstrumentedTest.java
+++ TwoActivities/app/src/androidTest/java/com/example/android/twoactivities/ExampleInstrumentedTest.java
@@ -18,11 +18,23 @@ package com.example.android.twoactivities;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -32,11 +44,34 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.android.twoactivities", appContext.getPackageName());
+    }
+
+    @Test
+    public void activityLaunch() {
+        onView(withId(R.id.button_main)).perform(click());
+        onView(withId(R.id.text_header)).check(matches(isDisplayed()));
+
+        onView((withId(R.id.button_second))).perform(click());
+        onView(withId(R.id.text_header_reply)).check(matches(isDisplayed()));
+
+    }
+
+    @Test
+    public void testInputOutput () {
+        onView(withId(R.id.editText_main)).perform(typeText("Hello World!"));
+        onView(withId(R.id.button_main)).perform(click());
+        onView(withId(R.id.text_message)).check(matches(withText("Hello World!")));
     }
 }
